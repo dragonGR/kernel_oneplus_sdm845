be318e8229b129192869c7792679dca384f8c83d
dragonGR <alex@dragongr.dev>
2022-07-13 23:46:14 +0300

    Revert "[PORT] drivers/power: Add interface for battery idle mode"
    This reverts commit e481bc9f69717722ffef67b2a9cdcd0fbf409098.

--- drivers/power/supply/qcom/bq27541_fuelgauger.c
+++ drivers/power/supply/qcom/bq27541_fuelgauger.c
@@ -231,10 +231,8 @@ struct bq27541_device_info {
 	int retry_count;
 	/*  Add for get right soc when sleep long time */
 	int soc_pre;
-	int batt_vol_pre;
+	int  batt_vol_pre;
 	int current_pre;
-	int cap_pre;
-	int remain_pre;
 	int health_pre;
 	int get_over_temp;
 	unsigned long rtc_resume_time;
@@ -267,10 +265,6 @@ struct bq27541_device_info {
 
 /*add by yangrujin@bsp 2016/3/16, reduce bq resume time*/
 #include <linux/workqueue.h>
-
-/* add to update fg node value on panel event */
-int panel_flag1;
-int panel_flag2;
 
 struct update_pre_capacity_data {
 	struct delayed_work work;
@@ -902,11 +896,7 @@ static int bq27541_remaining_capacity(struct bq27541_device_info *di)
 	int ret;
 	int cap = 0;
 
-	/* Add for get right soc when sleep long time */
-	if (atomic_read(&di->suspended) == 1)
-		return di->remain_pre;
-
-	if (di->allow_reading || panel_flag1) {
+	if (di->allow_reading) {
 #ifdef CONFIG_GAUGE_BQ27411
 		/* david.liu@bsp, 20161004 Add BQ27411 support */
 		ret = bq27541_read(di->cmd_addr.reg_rm,
@@ -918,13 +908,8 @@ static int bq27541_remaining_capacity(struct bq27541_device_info *di)
 			pr_err("error reading capacity.\n");
 			return ret;
 		}
-		if (panel_flag1)
-			panel_flag1 = 0;
-	} else {
-		return di->remain_pre;
 	}
 
-	di->remain_pre = cap;
 	return cap;
 }
 
@@ -933,11 +918,7 @@ static int bq27541_full_chg_capacity(struct bq27541_device_info *di)
 	int ret;
 	int cap = 0;
 
-	/* Add for get right soc when sleep long time */
-	if (atomic_read(&di->suspended) == 1)
-		return di->cap_pre;
-
-	if (di->allow_reading || panel_flag2) {
+	if (di->allow_reading) {
 #ifdef CONFIG_GAUGE_BQ27411
 		/* david.liu@bsp, 20161004 Add BQ27411 support */
 		ret = bq27541_read(BQ27411_REG_FCC,
@@ -949,13 +930,8 @@ static int bq27541_full_chg_capacity(struct bq27541_device_info *di)
 			pr_err("error reading full chg capacity.\n");
 			return ret;
 		}
-		if (panel_flag2)
-			panel_flag2 = 0;
-	} else {
-		return di->cap_pre;
 	}
 
-	di->cap_pre = cap;
 	return cap;
 }
 
